d30f9a27820249a64f28717b6aea3b655bcc0930
Tresa Horney <tresa.e@gmail.com>
2014-05-22 14:08:44 -0700

    updated form to live update in index.html and added Review Controller

--- app.js
+++ app.js
@@ -28,7 +28,13 @@
   //       this.current = 0; 
   //     } 
   // };
-
+app.controller('ReviewController', function() {
+    this.review = {};
+    this.addReview = function(product){
+    	product.reviews.push(this.review);
+     	this.review = {};
+    };
+});
   var gems = [
     {
       name: 'Azurite',
