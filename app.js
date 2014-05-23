590f5be115737b33925d43c3ac739dd22ca2053c
Tresa Horney <tresa.e@gmail.com>
2014-05-22 19:08:39 -0700

    updated app.js, index.html, added product-gallery.html

--- app.js
+++ app.js
@@ -1,39 +1,11 @@
 (function() {
-  var app = angular.module('gemStore', []);
-
+  var app = angular.module('gemStore', ['product-directives']);
 
   app.controller('StoreController', function(){
     this.products = gems;
   });
 
-app.directive('productTabs', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-tabs.html',
-      controller: function() { 
-        this.tab = 1;
-        this.isSet = function(checkTab) {
-          return this.tab === checkTab;
-        };
-        this.setTab = function(activeTab) {
-          this.tab = activeTab;
-    		};
-  		},
-      controllerAs: 'tab'
-  	};
-  });
-
-  //   app.controller('GalleryController', function() {
-		// this.current = 0;
-  //   this.setCurrent = function(newValue) {
-  //      if(newValue !== null) {
-  //       this.current = newValue; 
-  //     }
-  //     else{
-  //       this.current = 0; 
-  //     } 
-  // };
-app.controller('ReviewController', function() {
+  app.controller('ReviewController', function() {
     this.review = {};
     this.review.createdOn = Date.now();
 
@@ -42,28 +14,6 @@ app.controller('ReviewController', function() {
      	this.review = {};
     };
 });
-  
-app.directive("productDescription", function() {
-             return {
-                restrict: 'E',
-                templateUrl: "templates/product-description.html" 
-                };
-  });
-
-app.directive("productReviews", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "templates/product-reviews.html"
-    };
-  });
-
-  app.directive("productSpecs", function() {
-    return {
-      restrict:"A",
-      templateUrl: "templates/product-specs.html"
-    };
-  });
-
 
   var gems = [
     {
